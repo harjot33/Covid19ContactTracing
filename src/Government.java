bb1c95f2876e17c2f81f7fe377ceed1c8504ab7e
Harjot Singh <harjot.sidhu33@gmail.com>
2021-04-14 23:39:17 +0530

    Third Commit

--- src/Government.java
+++ src/Government.java
@@ -1,2 +1,239 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.StringReader;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.Date;
+
 public class Government {
+    Properties configFile;
+    ResultSet resultSet = null; // Initializing resultSet with null
+    Statement statement = null; // Initializing statement with null
+
+    Government(Properties configFile){
+            this.configFile = configFile;
+    }
+
+    boolean mobileContact(String initiator, String contactInfo) throws SQLException, ParserConfigurationException, IOException, SAXException, ParseException {
+        ConnectionEshtablisher();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse( new InputSource( new StringReader(contactInfo)));
+        boolean notest = false;
+        String sourceHash = document.getElementsByTagName("sourceHash").item(0).getTextContent();
+
+
+        if (!resultSet.isBeforeFirst() ) {
+
+
+            NodeList testlist = document.getElementsByTagName("testHash");
+            String testhash="";
+            ArrayList<String> contactlist = new ArrayList<>();
+            boolean singulartest = false;
+            String positive_contact ="";
+            String pcontact_date = "";
+            String positive_status = "";
+            String ptest_date = "";
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+            String exception_date = "";
+            Calendar calendar = Calendar.getInstance();
+
+            if(!document.getElementsByTagName("testHash").item(0).getTextContent().isEmpty()){
+                ArrayList<String> thashlist = new ArrayList<>();
+
+                for(int i = 0 ; i<testlist.getLength(); i++){
+                    testhash = document.getElementsByTagName("testHash").item(i).getTextContent();
+                    //    System.out.println(testhash);
+                    resultSet = statement.executeQuery("Select TestHash,testDate, TestResult from TestRecord where TestHash ='"+testhash+"';");
+
+                    if(resultSet.isBeforeFirst())
+                        resultSet.next();
+                        thashlist.add(resultSet.getString("TestHash"));
+                }
+                System.out.println(thashlist);
+
+                if(thashlist.size()==1 && thashlist.get(0).equals(document.getElementsByTagName("testHash").item(0).getTextContent())){
+                    String govtresult = resultSet.getString("TestResult");
+                    if(govtresult.equals("true")){
+                        System.out.println("TEST HAS BEEN VALIDATED");
+                        singulartest  = true;
+                    }
+
+                }else if(thashlist.size()>1){
+                    Map<Integer, String> testdateregister = new HashMap<>();
+                    ArrayList<Integer> keytracker = new ArrayList<>();
+                    ArrayList<String> ExceptionDate = new ArrayList<>();
+
+                    for(int i =0 ; i<thashlist.size();i++){
+                        testhash = thashlist.get(0);
+                        resultSet = statement.executeQuery("Select TestHash,testDate, TestResult from TestRecord where TestHash ='"+testhash+"';");
+                        String govtresult = resultSet.getString("TestResult");
+                        if(govtresult.equals("true")){
+                            System.out.println("TEST HAS BEEN VALIDATED");
+                            String date = resultSet.getString("testDate");
+                            testdateregister.put(i,date);
+                            keytracker.add(i);
+                        }
+                    }
+                    String smalldate = testdateregister.get(0);
+                    Date firstdate = new Date();
+                    Date date1 = sdf.parse(smalldate);
+                    for(int i =1 ; i<testdateregister.size();i++){
+                        Date date2 = sdf.parse(testdateregister.get(i));
+                        if(date2.after(date1)){
+                            firstdate = date1;
+                            calendar.setTime(date1);
+                            calendar.add(Calendar.DAY_OF_MONTH,14);
+                            String futuredate = sdf.format(calendar.getTime());
+                            Date negativedate = sdf.parse(futuredate);
+                            String entry = sdf.format(negativedate);
+
+                            if(negativedate.before(date2)){
+                                ExceptionDate.add(entry);
+                            }
+
+                        }else{
+                            firstdate = date2;
+                            date1 = date2;
+                            calendar.setTime(firstdate);
+                            calendar.add(Calendar.DAY_OF_MONTH,14);
+                            String futuredate = sdf.format(calendar.getTime());
+                            Date negativedate = sdf.parse(futuredate);
+                            String entry = sdf.format(negativedate);
+
+                            if(negativedate.before(date1)){
+                                ExceptionDate.add(entry);
+                            }
+                        }
+                    }
+                    Date final_date = sdf.parse("01-01-2021");
+                    if(ExceptionDate.size()!=0){
+                        for(int i =0 ; i<ExceptionDate.size();i++){
+                            Date current_date = sdf.parse(ExceptionDate.get(i));
+
+                            if(current_date.after(final_date)){
+                                final_date = current_date;
+                            }
+                        }
+                        exception_date = sdf.format(final_date);
+
+                        pcontact_date = sdf.format(firstdate);
+                        positive_contact = "SELF";
+                        ptest_date = sdf.format(firstdate);
+                        positive_status="true";
+                    }else{
+                        pcontact_date = sdf.format(firstdate);
+                        positive_contact = "SELF";
+                        ptest_date = sdf.format(firstdate);
+                        positive_status="true";
+                    }
+
+
+                }
+            }else{
+                notest = true;
+            }
+
+            NodeList ok = document.getElementsByTagName("device");
+
+         //   System.out.println(document.getElementsByTagName("testHash").item(0).getTextContent());
+            for(int i =0 ; i < ok.getLength(); i++){
+                        String deviceHash  =document.getElementsByTagName("deviceHash").item(i).getTextContent();
+                        contactlist.add(deviceHash);
+                        String date = document.getElementsByTagName("date").item(i).getTextContent();
+                        String duration = document.getElementsByTagName("duration").item(i).getTextContent();
+                        System.out.println(deviceHash+"-"+date+"-"+duration+"-"+positive_status);
+                    }
+
+
+
+            // statement.executeUpdate("INSERT INTO devicerecord (devicehash, contact_list, last_contact)\n" +"Values('" + sourceHash + "','" + contactlistHash + "','"+result+"')");
+            String last_contact = contactlist.get(contactlist.size()-1);
+            if(singulartest){
+              //  positive_contact = resultSet.getString("")
+                ptest_date = resultSet.getString("testDate");
+                positive_status = resultSet.getString("TestResult");
+                positive_contact = "SELF";
+                pcontact_date = ptest_date;
+                String contactstring = String.join(",", contactlist);
+                statement.executeUpdate("INSERT INTO devicerecord (devicehash, contact_list, last_contact,positive_contact, pcontact_date, positive_status, ptest_date)" +"Values('" + sourceHash + "','" + contactstring + "','"+last_contact+"','"+positive_contact+"','"+pcontact_date+"','"+positive_status+"','"+ptest_date+"')");
+            }else if(notest){
+                ptest_date = "null";
+                positive_status = "false";
+                positive_contact = "null";
+                pcontact_date = "null";
+                String contactstring = String.join(",", contactlist);
+                statement.executeUpdate("INSERT INTO devicerecord (devicehash, contact_list, last_contact,positive_contact, pcontact_date, positive_status, ptest_date)" +"Values('" + sourceHash + "','" + contactstring + "','"+last_contact+"','"+positive_contact+"','"+pcontact_date+"','"+positive_status+"','"+ptest_date+"')");
+
+
+            }
+
+
+        }else {
+            resultSet = statement.executeQuery("Select * from devicerecord where devicehash='"+ sourceHash+"';");
+            resultSet.next();
+            System.out.println(resultSet.getString("devicehash"));
+
+
+
+
+        }
+
+
+
+
+        return true;
+    }
+
+
+    private boolean ConnectionEshtablisher(){
+
+        Connection connection = null; // Initializing connection with null
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Setting up the jdbc driver to connect
+        } catch (Exception e) {
+            System.out.println("Error Connecting to jdbc"); // If there is any exception, we display this message
+        }
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://db.cs.dal.ca:3306/harjot", "harjot", "B00872298"); // Setting up the connection with the server url and the user and password
+            statement = connection.createStatement(); // The statement is then created on the basis of the connection
+            resultSet = statement.executeQuery("select * from devicerecord;");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+        return true;
+    }
+
+    public void recordTestResult(String testHash, int date, boolean result) throws SQLException {
+        if(testHash.isEmpty() || testHash==null || date < 1 ){
+            System.out.println("Invalid Test Hash or Date");
+            return;
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar calendar = Calendar.getInstance();
+        try {
+            calendar.setTime(sdf.parse("01-01-2021"));
+        }catch (ParseException pe){
+            System.out.println(pe);
+        }
+
+        calendar.add(Calendar.DAY_OF_MONTH,date);
+        String dateofcontact = sdf.format(calendar.getTime());
+
+        statement.executeUpdate("Insert into TestRecord (TestHash, testDate, TestResult) Values('" + testHash + "','" + dateofcontact + "','"+result+"')");
+    }
+
+
 }
--- src/MobileDevice.java
+++ src/MobileDevice.java
@@ -1,15 +1,26 @@
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 
@@ -23,9 +34,11 @@ public class MobileDevice {
     int synchchecker=0;
     HashMap<String, ArrayList<String >> testrecord = new HashMap<>();
     ArrayList<String> TestList = new ArrayList<>();
+    String initiator = "";
 
     MobileDevice(Properties configFile, Government contactTracer)  {
             this.configFile = configFile;
+            initiator = configFile.getProperty("deviceHash");
             this.contactTracer = contactTracer;
 
     }
@@ -47,8 +60,8 @@ public class MobileDevice {
         p.setProperty("deviceHash",individual);
         p.setProperty("date",dateofcontact);
         p.setProperty("duration",""+duration);
-        contact_instance ++;
         contactList.put(contact_instance,p);
+        contact_instance ++;
         System.out.println("DEVICE NAME- "+configFile.getProperty("deviceName"));
         System.out.println(contactList);
 
@@ -69,24 +82,46 @@ public class MobileDevice {
 
     }
 
-    public boolean synchronizeData() throws ParserConfigurationException {
+    public boolean synchronizeData() throws Exception {
         DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance(); // Using the DOM DocumentBuilderFactory's instance to create a documentfactory object
         DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder(); // We create a new documentBuilder
         Document document = documentBuilder.newDocument();
         Element information = document.createElement("information"); //We create the root element of the document tree
         document.appendChild(information); // We append it to the document tree as the root child
 
-        Element device_hash = document.createElement("device_hash"); //We create the root element of the document tree
-        device_hash.appendChild(document.createTextNode(configFile.getProperty("deviceHash"))); // We set the child of this node as a textnode, depicting some text value
-        information.appendChild(device_hash); // We append it to the document tree as the root child
+        Element sourcedevice_hash = document.createElement("sourceHash"); //We create the root element of the document tree
+        sourcedevice_hash.appendChild(document.createTextNode(configFile.getProperty("deviceHash"))); // We set the child of this node as a textnode, depicting some text value
+        information.appendChild(sourcedevice_hash); // We append it to the document tree as the root child
+
+        Element test_Hash = document.createElement("testHash"); //We create the root element of the document tree
+        for (int i = 0 ; i<TestList.size() ; i++) {
+            test_Hash.appendChild(document.createTextNode(TestList.get(i))); // We set the child of this node as a textnode, depicting some text value
+        }
+        information.appendChild(test_Hash); // We append it to the document tree as the root child
 
         Element contacts_list = document.createElement("contact_list"); //We create the root element of the document tree
-        for(int i = synchchecker ; i<contactList.size();i++){
-            contacts_list.appendChild(document.createTextNode(configFile.getProperty("deviceHash"))); // We set the child of this node as a textnode, depicting some text value
-        }
         information.appendChild(contacts_list); // We append it to the document tree as the root child
+        for(int i = 0 ; i<contactList.size();i++){
+            Element device = document.createElement("device");
+            device.appendChild(document.createTextNode(i+""));
+            contacts_list.appendChild(device);
+            Element deviceHash = document.createElement("deviceHash");
+            deviceHash.appendChild(document.createTextNode(contactList.get(i).getProperty("deviceHash")));
+            device.appendChild(deviceHash);
+            Element date = document.createElement("date");
+            date.appendChild(document.createTextNode(contactList.get(i).getProperty("date")));
+            device.appendChild(date);
+            Element duration = document.createElement("duration");
+            duration.appendChild(document.createTextNode(contactList.get(i).getProperty("duration")));
+            device.appendChild(duration);
+        }
 
 
+        Transformer transformer = TransformerFactory.newInstance().newTransformer(); // Initializing Transformer Object with an instance of TransformerFactory
+        StringWriter writer = new StringWriter();
+        transformer.transform(new DOMSource(document), new StreamResult(writer));
+        String contactInfo = writer.getBuffer().toString();
+        contactTracer.mobileContact(initiator,contactInfo);
         return true;
     }
 
@@ -96,32 +131,46 @@ public class MobileDevice {
         byte[] sha256 = md.digest(deviceName.getBytes(StandardCharsets.UTF_8));
         BigInteger sig = new BigInteger(1, sha256);
         String deviceHash = sig.toString(16);
-        System.out.println(deviceHash);
+        //System.out.println(deviceHash);
         return deviceHash;
     }
 
-    public static void main(String[] args) throws NoSuchAlgorithmException {
-        Government contactTracer = new Government();
+
+
+
+    public static void main(String[] args) throws Exception {
+        Properties govtproperties = new Properties();
+        govtproperties.setProperty("user","harjot");
+        govtproperties.setProperty("password","B00872298");
+        govtproperties.setProperty("url","jdbc:mysql://db.cs.dal.ca:3306/harjot");
+        Government contactTracer = new Government(govtproperties);
         Properties config = new Properties();
         config.setProperty("address", "127.192.1.1");
         config.setProperty("deviceName", "Oneplus 3T");
-        Random ID_No = new Random();
-        int Mobile_ID = ID_No.nextInt(9000000) + 1000000;
-        String unique = config.getProperty("address") + config.getProperty("deviceName") + Mobile_ID;
+        String unique = config.getProperty("address") + config.getProperty("deviceName");
         String deviceHash1 = HashGenerator(unique);
         config.setProperty("deviceHash", deviceHash1);
         MobileDevice device1 = new MobileDevice(config, contactTracer);
         Properties config2 = new Properties();
         config2.setProperty("address", "117.192.1.1");
         config2.setProperty("deviceName", "Samsung Galaxy");
-        Mobile_ID = ID_No.nextInt(9000000) + 1000000;
-        unique = config2.getProperty("address") + config2.getProperty("deviceName") + Mobile_ID;
+        unique = config2.getProperty("address") + config2.getProperty("deviceName");
         String deviceHash2 = HashGenerator(unique);
         config2.setProperty("deviceHash", deviceHash2);
         MobileDevice device2 = new MobileDevice(config2,contactTracer);
         device1.recordContact(deviceHash2,31,2);
         device2.recordContact(deviceHash1,31,2);
-        device1.recordContact("3333",31,2);
+        Properties config3 = new Properties();
+        config3.setProperty("address", "217.292.11.1");
+        config3.setProperty("deviceName", "iPhone 11 Pro");
+        unique = config3.getProperty("address") + config3.getProperty("deviceName");
+        String deviceHash3 = HashGenerator(unique);
+        config3.setProperty("deviceHash", deviceHash3);
+        MobileDevice device3 = new MobileDevice(config3,contactTracer);
+        device1.recordContact(deviceHash3,21,30);
+     device1.positiveTest(HashGenerator("2323AACCVVV"));
+       device1.synchronizeData();
+       // contactTracer.recordTestResult(HashGenerator("2323AACCVVV"), 23, true);
 
     }
 }
